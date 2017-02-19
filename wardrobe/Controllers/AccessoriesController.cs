53b36c5d8f6a613c6f3c4f1e6de8ff56098a13a8
Merichu Duncan <chue_3@yahoo.com>
2017-02-19 14:46:28 -0500

    Added my Models, Controllers and trying to style my Views.

---
+++ wardrobe/Controllers/AccessoriesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using wardrobe.Models;
+
+namespace wardrobe.Controllers
+{
+    public class AccessoriesController : Controller
+    {
+        private WardrobeEntities db = new WardrobeEntities();
+
+        // GET: Accessories
+        public ActionResult Index()
+        {
+            return View(db.Accessories1.ToList());
+        }
+
+        // GET: Accessories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Accessories accessories = db.Accessories1.Find(id);
+            if (accessories == null)
+            {
+                return HttpNotFound();
+            }
+            return View(accessories);
+        }
+
+        // GET: Accessories/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Accessories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AccessoriesID,NameofAccessories,Photo")] Accessories accessories)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Accessories1.Add(accessories);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(accessories);
+        }
+
+        // GET: Accessories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Accessories accessories = db.Accessories1.Find(id);
+            if (accessories == null)
+            {
+                return HttpNotFound();
+            }
+            return View(accessories);
+        }
+
+        // POST: Accessories/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AccessoriesID,NameofAccessories,Photo")] Accessories accessories)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(accessories).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(accessories);
+        }
+
+        // GET: Accessories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Accessories accessories = db.Accessories1.Find(id);
+            if (accessories == null)
+            {
+                return HttpNotFound();
+            }
+            return View(accessories);
+        }
+
+        // POST: Accessories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Accessories accessories = db.Accessories1.Find(id);
+            db.Accessories1.Remove(accessories);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ wardrobe/Controllers/WardrobesController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using wardrobe.Models;
+
+namespace wardrobe.Controllers
+{
+    public class WardrobesController : Controller
+    {
+        private WardrobeEntities db = new WardrobeEntities();
+
+        // GET: Wardrobes
+        public ActionResult Index()
+        {
+            var wardrobes = db.Wardrobes.Include(w => w.Accessory).Include(w => w.Bottom).Include(w => w.Sho).Include(w => w.Top);
+            return View(wardrobes.ToList());
+        }
+
+        // GET: Wardrobes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Wardrobe wardrobe = db.Wardrobes.Find(id);
+            if (wardrobe == null)
+            {
+                return HttpNotFound();
+            }
+            
+            
+            return View(wardrobe);
+        }
+
+        // GET: Wardrobes/Create
+        public ActionResult Create()
+        {
+            ViewBag.AccessoriesID = new SelectList(db.Accessories1, "AccessoriesID", "NameofAccessories");
+            ViewBag.BottomsID = new SelectList(db.Bottoms, "BottomsID", "BottomStyle");
+            ViewBag.ShoesID = new SelectList(db.Shoes1, "ShoesID", "ShoeStyle");
+            ViewBag.TopsID = new SelectList(db.Tops, "TopsID", "TopStyle");
+            return View();
+        }
+
+        // POST: Wardrobes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "WardrobeID,Name,Occasion,Season,TopsID,BottomsID,ShoesID,AccessoriesID")] Wardrobe wardrobe)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Wardrobes.Add(wardrobe);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AccessoriesID = new SelectList(db.Accessories1, "AccessoriesID", "NameofAccessories", wardrobe.AccessoriesID);
+            ViewBag.BottomsID = new SelectList(db.Bottoms, "BottomsID", "BottomStyle", wardrobe.BottomsID);
+            ViewBag.ShoesID = new SelectList(db.Shoes1, "ShoesID", "ShoeStyle", wardrobe.ShoesID);
+            ViewBag.TopsID = new SelectList(db.Tops, "TopsID", "TopStyle", wardrobe.TopsID);
+            return View(wardrobe);
+        }
+
+        // GET: Wardrobes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Wardrobe wardrobe = db.Wardrobes.Find(id);
+            if (wardrobe == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AccessoriesID = new SelectList(db.Accessories1, "AccessoriesID", "NameofAccessories", wardrobe.AccessoriesID);
+            ViewBag.BottomsID = new SelectList(db.Bottoms, "BottomsID", "BottomStyle", wardrobe.BottomsID);
+            ViewBag.ShoesID = new SelectList(db.Shoes1, "ShoesID", "ShoeStyle", wardrobe.ShoesID);
+            ViewBag.TopsID = new SelectList(db.Tops, "TopsID", "TopStyle", wardrobe.TopsID);
+            return View(wardrobe);
+        }
+
+        // POST: Wardrobes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "WardrobeID,Name,Occasion,Season,TopsID,BottomsID,ShoesID,AccessoriesID")] Wardrobe wardrobe)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(wardrobe).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AccessoriesID = new SelectList(db.Accessories1, "AccessoriesID", "NameofAccessories", wardrobe.AccessoriesID);
+            ViewBag.BottomsID = new SelectList(db.Bottoms, "BottomsID", "BottomStyle", wardrobe.BottomsID);
+            ViewBag.ShoesID = new SelectList(db.Shoes1, "ShoesID", "ShoeStyle", wardrobe.ShoesID);
+            ViewBag.TopsID = new SelectList(db.Tops, "TopsID", "TopStyle", wardrobe.TopsID);
+            return View(wardrobe);
+        }
+
+        // GET: Wardrobes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Wardrobe wardrobe = db.Wardrobes.Find(id);
+            if (wardrobe == null)
+            {
+                return HttpNotFound();
+            }
+            return View(wardrobe);
+        }
+
+        // POST: Wardrobes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Wardrobe wardrobe = db.Wardrobes.Find(id);
+            db.Wardrobes.Remove(wardrobe);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
